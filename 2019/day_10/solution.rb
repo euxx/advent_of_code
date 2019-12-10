c910166ad483e0b7b2e6557d3efc4be10efd327d
eux <euxuuu@gmail.com>
2019-12-10 23:29:15 +0800

    Improve 2019 day 10-1

--- 2019/day_10/solution.rb
+++ 2019/day_10/solution.rb
@@ -12,24 +12,9 @@ STARS = DATA.each_with_index.reduce([]) do |stars, (line, y)|
   stars + parse(line, y)
 end
 
-def block?(star, another_star, any_star)
-  x1, y1 = star
-  x2, y2 = any_star
-  x3, y3 = another_star
-
-  (x2 - x1) * (y3 - y1) == (x3 - x1) * (y2 - y1) &&
-  ((x1 - x2) * (x2 - x3) > 0 || (y1 - y2) * (y2 - y3) > 0)
-end
-
-def detected?(star, another_star, remain_stars)
-  !remain_stars.any? { |any_star| block?(star, another_star, any_star) }
-end
-
 def detected_stars_count(star)
-  other_stars = STARS - [star]
-  other_stars.count do |another_star|
-    detected?(star, another_star, other_stars - [another_star])
-  end
+  x1, y1 = star
+  (STARS - [star]).group_by { |x2, y2| Math.atan2(x1 - x2, y1 - y2) }.count
 end
 
 answer1 = STARS.map(&method(:detected_stars_count)).max
