9dd52297e824ad48c4ac3fb450ac74b542edd94e
jianglan <euxuuu@gmail.com>
2019-12-11 14:01:00 +0800

    Complete 2019 day 10-2

--- 2019/day_10/solution.rb
+++ 2019/day_10/solution.rb
@@ -12,17 +12,31 @@ STARS = DATA.each_with_index.reduce([]) do |stars, (line, y)|
   stars + parse(line, y)
 end
 
-def detected_stars_count(star)
+def detected_stars(star)
   x1, y1 = star
-  (STARS - [star]).group_by { |x2, y2| Math.atan2(x1 - x2, y1 - y2) }.count
+  (STARS - [star]).group_by { |x2, y2| Math.atan2(x1 - x2, y1 - y2) }
+    .map { |_, stars| stars.min_by { |x3, y3| (x1 - x3).abs + (y1 - y3).abs } }
 end
 
-answer1 = STARS.map(&method(:detected_stars_count)).max
+best_location = STARS.max_by { |star| detected_stars(star).size }
+max_detected_stars = detected_stars(best_location)
+
+answer1 = max_detected_stars.size
 
 puts "Part One - The puzzle answer is #{answer1}"
 
 # Part Two
 
-answer2 = nil
+NTH = 200
+
+x0, y0 = best_location
+sorted_stars = max_detected_stars.sort_by do |x, y|
+  theta = Math.atan2(x - x0, y0 - y) * 180 / Math::PI
+  theta < 0 ? theta + 360 : theta
+end
+
+x, y = sorted_stars[NTH - 1]
+
+answer2 = x * 100 + y
 
 puts "Part Two - The puzzle answer is #{answer2}"
