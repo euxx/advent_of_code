99aaee83bae8862cfc35ac07838bce5be5ae9ba4
jianglan <euxuuu@gmail.com>
2020-12-23 17:27:15 +0800

    w

--- 2020/day_23/solution.rb
+++ 2020/day_23/solution.rb
@@ -1,33 +1,70 @@
-input = 925176834
+input = 389125467
 
 cups = input.digits.reverse
 
-def move(cups, n)
-  size = n > 100 ? 1000000 : cups.size
-  cups = n > 100 ? cups + (10..size).to_a : cups.dup
-  i = 0
+require "pry"
 
-  n.times do
-    current = cups[i]
-    i1, i2, i3 = (i + 1) % size, (i + 2) % size, (i + 3) % size
-    pick_up = [cups[i1], cups[i2], cups[i3]]
-    cups -= pick_up
+class Cup
+  attr_accessor :value, :next
 
-    min, max = cups.minmax
-    dest = current - 1
+  def initialize(value)
+    @value = value
+  end
+
+  def list
+    a = [self]
+    o = self.next
+    until o == self
+      a << o
+      o = o.next
+    end
+    a
+  end
+
+  def values
+    list.map(&:values)
+  end
+end
+
+def move(cups, n = 10, huge: false)
+  size = huge ? 1000000 : cups.size
+  cups = huge ? cups + (10..size).to_a : cups.dup
+
+  objects = cups.map { |c| Cup.new(c) }
+  objects.each_with_index do |o, idx|
+    o.next = objects[(idx + 1) % size]
+  end
+  current = objects.first
+  # binding.pry
+
+  n.times do |i|
+    i1, i2, i3 = current.next, current.next.next, current.next.next.next
+    pick_up = [i1, i2, i3]
+    current.next = i3.next
+
+    list = current.list
+    values = list.map(&:value)
+    min, max = values.minmax
+    dest = current.value - 1
     dest = max if dest < min
 
-    if pick_up.include?(dest)
-      while pick_up.include?(dest)
+    pick_up_values = pick_up.map(&:value)
+
+    if pick_up_values.include?(dest)
+      while pick_up_values.include?(dest)
         dest -= 1
         dest = max if dest < min
       end
     end
 
-    index = cups.index(dest)
-    cups.insert((index + 1) % size, *pick_up)
 
-    i = (cups.index(current) + 1) % size
+    d_o = list.find { |oo| oo.value == dest }
+    old = d_o.next
+    d_o.next = i1
+    i3.next = old
+    # binding.pry
+
+    current = current.next
   end
 
   cups
@@ -36,16 +73,16 @@ end
 # Part One
 
 final = move(cups, 100)
-i = final.index(1)
+i = final.find { |oo| oo.value == 1 }
 
-result = (final + final)[i+ 1..i + final.size - 1].join
+result = i.next.list.map(&:value).join
 
 puts "Part One - The puzzle answer is #{result}"
 
 # Part Two
 
-final = move(cups, 10000000)
-i = final.index(1)
+# final = move(cups, 10000000)
+# i = final.index(1)
 
 result = final[i + 1] * final[i + 2]
 
