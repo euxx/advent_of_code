906976df81d14c295db4aa9829e75e5b14c441a8
euxx <euxuuu@gmail.com>
2021-01-12 12:42:24 +0800

    Improve 2020 day 1

--- 2020/day_01/solution.rb
+++ 2020/day_01/solution.rb
@@ -4,8 +4,7 @@ numbers = input.map(&:to_i)
 
 # Part One
 
-array_two = numbers.product(numbers)
-the_pair = array_two.find { |x, y| x + y == 2020 }
+the_pair = numbers.combination(2).find { |pair| pair.sum == 2020 }
 
 result = the_pair.inject(:*)
 
@@ -13,9 +12,8 @@ puts "Part One - The puzzle answer is #{result}"
 
 # Part Two
 
-array_three = array_two.product(numbers)
-the_three = array_three.find { |(x, y), z| x + y + z == 2020 }
+the_three = numbers.combination(3).find { |three| three.sum == 2020 }
 
-result = the_three.flatten.inject(:*)
+result = the_three.inject(:*)
 
 puts "Part Two - The puzzle answer is #{result}"
