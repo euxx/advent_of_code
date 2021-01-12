6712be863fe8fc47ec716a36e051cde05f30da47
euxx <euxuuu@gmail.com>
2021-01-12 12:51:29 +0800

    Improve 2020 day 1 with Ruby 3

--- 2020/day_01/solution.rb
+++ 2020/day_01/solution.rb
@@ -1,19 +1,17 @@
 input = File.readlines("./2020/day_01/input.txt")
 
-numbers = input.map(&:to_i)
+NUMBERS = input.map(&:to_i)
+
+def calc(n) = NUMBERS.combination(n).find { _1.sum == 2020 }.reduce(:*)
 
 # Part One
 
-the_pair = numbers.combination(2).find { |pair| pair.sum == 2020 }
-
-result = the_pair.inject(:*)
+result = calc(2)
 
 puts "Part One - The puzzle answer is #{result}"
 
 # Part Two
 
-the_three = numbers.combination(3).find { |three| three.sum == 2020 }
-
-result = the_three.inject(:*)
+result = calc(3)
 
 puts "Part Two - The puzzle answer is #{result}"
