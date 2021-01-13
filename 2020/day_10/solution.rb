d96c30ece918cf0836fbfecacd4346e1d4ecd11a
euxx <euxuuu@gmail.com>
2021-01-13 16:41:55 +0800

    Improve 2020 day 10

--- 2020/day_10/solution.rb
+++ 2020/day_10/solution.rb
@@ -1,36 +1,19 @@
 input = File.readlines("./2020/day_10/input.txt")
 
-numbers = input.map { |line| line.delete("\n").to_i }.sort
+numbers = input.map(&:to_i).sort
 
 # Part One
 
-combinations = numbers.zip([0] + numbers[0..-2])
-differences = combinations.map { |x, y| x - y }
+differences = [0, *numbers, numbers.max + 3].each_cons(2).map { |x, y| y - x }
 
-count_diff_1 = differences.count(1)
-count_diff_3 = differences.count(3) + 1
-
-result = count_diff_1 * count_diff_3
+result = differences.count(1) * differences.count(3)
 
 puts "Part One - The puzzle answer is #{result}"
 
 # Part Two
 
-diff_1_count_group = [] # A group of contiguous_diff_1 count
-diff_1_count = 0
-last_index = differences.size - 1
-
-differences.each_with_index do |number, index|
-  if number == 1
-    diff_1_count += 1
-    diff_1_count_group << diff_1_count if index == last_index
-  else
-    diff_1_count_group << diff_1_count if diff_1_count > 1
-    diff_1_count = 0
-  end
-end
-
-ways_count_group = diff_1_count_group.map do |count|
+ways_count_group = differences.join.scan(/1+/).map do |ones|
+  count = ones.size
   max = count - 2
   count + (1 + max) * max / 2
 end
