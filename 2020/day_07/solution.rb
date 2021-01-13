38339109b2e6cb820348bbfdbea104039531471e
euxx <euxuuu@gmail.com>
2021-01-13 14:32:33 +0800

    Improve 2020 day 7

--- 2020/day_07/solution.rb
+++ 2020/day_07/solution.rb
@@ -2,25 +2,10 @@ input = File.readlines("./2020/day_07/input.txt")
 
 RULES = # {color_a => {color_b => n1, color_c => n2}}
   input.each_with_object({}) do |line, rules|
-    line = line.delete("\n")
-    string = ' contain no other bags.'
-    split_string = line.end_with?(string) ? string : ' contain '
-
-    hd, tl = line.split(split_string)
-    color = hd.split(' ')[0..-2].join(' ')
-    sub_rules =
-      if tl
-        tl[0..-2].split(', ')
-                 .map do |sub_rule|
-                    sub_rule = sub_rule.split(' ')
-                    sub_color = sub_rule[1..-2].join(' ')
-                    number = sub_rule[0].to_i
-
-                    [sub_color, number]
-                 end
-      else
-        []
-      end
+    color = line.scan(/\A\w+ \w+/).first
+    sub_rules = line.scan(/(\d+) (\w+ \w+)/).map do |n, c|
+      [c, n.to_i]
+    end
 
     rules[color] = sub_rules.to_h
   end
@@ -38,11 +23,8 @@ def cotain_any_shiny_gold?(colors)
   end
 end
 
-rules = RULES.select do |color, sub_rules|
-  colors = sub_rules.keys
-  next true if colors.include?('shiny gold')
-
-  cotain_any_shiny_gold?(colors)
+rules = RULES.each_value.select do |sub_rules|
+  cotain_any_shiny_gold?(sub_rules.keys)
 end
 
 result = rules.count
@@ -52,8 +34,6 @@ puts "Part One - The puzzle answer is #{result}"
 # Part Two
 
 def total_bags(sub_rules)
-  return 0 if sub_rules.empty?
-
   sub_rules.sum do |color, number|
     number + number * total_bags(RULES[color])
   end
