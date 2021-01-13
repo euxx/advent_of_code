521142210916cbdb16a848538ced5b2b43cbdb45
euxx <euxuuu@gmail.com>
2021-01-13 16:11:58 +0800

    Improve 2020 day 9

--- 2020/day_09/solution.rb
+++ 2020/day_09/solution.rb
@@ -1,15 +1,12 @@
 input = File.readlines("./2020/day_09/input.txt")
 
-numbers = input.map { |line| line.delete("\n").to_i }
+numbers = input.map(&:to_i)
 
 # Part One
 
-step = 25
-
-only_number = numbers[step..-1].find.with_index do |number, index|
-  prev_nums = numbers[index..index + step - 1]
-  !prev_nums.product(prev_nums).map(&:sum).include?(number)
-end
+only_number = numbers.each_cons(26).find do |*prev_nums, number|
+  prev_nums.combination(2).all? { _1.sum != number }
+end.last
 
 result = only_number
 
@@ -17,27 +14,15 @@ puts "Part One - The puzzle answer is #{result}"
 
 # Part Two
 
-index = 0
-found = false
-contiguous_numbers = []
+i1, i2 = 0, 0
 
-until found
-  sub_index = 0
-  finish = false
+loop do
+  sum = numbers[i1..i2].sum
+  break if sum == only_number && i1 < i2
 
-  until finish || found
-    contiguous_numbers = numbers[index..sub_index]
-    sum = contiguous_numbers.sum
-
-    found = true if sum == only_number
-    finish = true if sum > only_number
-
-    sub_index += 1
-  end
-
-  index += 1
+  sum > only_number ? i1 += 1 : i2 += 1
 end
 
-result = contiguous_numbers.minmax.sum
+result = numbers[i1..i2].minmax.sum
 
 puts "Part Two - The puzzle answer is #{result}"
